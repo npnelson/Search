39e4ceb367c9b6a15db7ea37ed5b427dbc0ee7fe
npnelson <nnelson1@insight.rr.com>
2020-09-25 12:30:02 -0400

    WIP

--- src/NetToolBox.Search.AzureCognitiveSearch/AzureSearchFactoryServiceCollectionExtensions.cs
+++ src/NetToolBox.Search.AzureCognitiveSearch/AzureSearchFactoryServiceCollectionExtensions.cs
@@ -6,6 +6,12 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AzureSearchFactoryServiceCollectionExtensions
     {
+        /// <summary>
+        /// Add AzureSearch Services
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configurationSection">Point to a section of the configuration that can be bound to AzureSearchSettings</param>
+        /// <returns></returns>
         public static IServiceCollection AddAzureSearch(this IServiceCollection services, IConfigurationSection configurationSection)
         {
             services.AddSingleton<AzureSearchFactory>();
